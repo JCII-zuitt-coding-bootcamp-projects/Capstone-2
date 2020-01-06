82716258db04068f56ed7d59686b07355fc845d5
Jerome Dacasin <jeromedacasin21@gmail.com>
2020-01-06 13:10:48 +0800

    u

--- resources/views/reservations.blade.php
+++ resources/views/reservations.blade.php
@@ -15,7 +15,10 @@
 
   <div class="container p-sm-0 p-md-5 " id="reservations">
     
-    <h6 class="text-info mt-5 text-center" v-if="reservation_payments.length == 0">You have no reservations.</h6>
+    <template v-if="reservation_payments != null">
+      <h6 class="text-info mt-5 text-center" v-if="reservation_payments.length == 0">You have no reservations.</h6>
+    </template>
+    
     <div v-for="payment in reservation_payments" class="mt-3">
         <template  v-if="payment.reservations.length > 0">
           <h6 class="text-info text-center">
